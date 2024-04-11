37a6a8d990c0200fdbf400018f94a089037c436a
Kristian Koribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-04-11 18:34:07 +0200

    problem: 145
    My iterative solution using two stacks with memory safety checks.

--- 145. Binary Tree Postorder Traversal.c
+++ 145. Binary Tree Postorder Traversal.c
@@ -12,32 +12,50 @@
 
 #define MAX_NODES 100
 
+void check_capacity(int current_index);
+
 int* postorderTraversal(struct TreeNode* root, int* returnSize) {
     
+    int stack_vals[MAX_NODES];
+    int stack_vals_top = 0;
+
+    struct TreeNode *stack_nodes[MAX_NODES];
+    int stack_nodes_top = 0;
+
+    struct TreeNode *current_node;
+    stack_nodes[stack_nodes_top++] = root;
+    while (stack_nodes_top > 0 && (current_node = stack_nodes[--stack_nodes_top]) != NULL) {
+        check_capacity(stack_vals_top);
+        stack_vals[stack_vals_top++] = current_node->val;
+
+        if (current_node->left != NULL) {
+            check_capacity(stack_nodes_top);
+            stack_nodes[stack_nodes_top++] = current_node->left;
+        }
+        if (current_node->right != NULL) {
+            check_capacity(stack_nodes_top);
+            stack_nodes[stack_nodes_top++] = current_node->right;
+        }
+    }
+
     int *array_vals = malloc(MAX_NODES * sizeof(int));
     if (array_vals == NULL) {
         fprintf(stderr, "Memory allocation error!");
         exit(EXIT_FAILURE);
     }
-    int array_size = 0;
+    int array_index = 0;
+    while(stack_vals_top > 0) {
+        check_capacity(array_index);
+        array_vals[array_index++] = stack_vals[--stack_vals_top];
+    }
 
-    struct TreeNode *stack_nodes[MAX_NODES];
-    int stack_top = 0;
+    *returnSize = array_index;
+    return array_vals;
+}
 
-    stack_nodes[stack_top++] = root;
-
-    struct TreeNode *current_node;
-    while(stack_top > 0 && (current_node = stack_nodes[--stack_top]) != NULL) {
-        // push child nodes onto the stack
-        if (root->right != NULL) {
-            if (stack_top == MAX_NODES) {
-                fprintf(stderr, "Maximum capacity of node stack reached!\n");
-                exit(EXIT_FAILURE);
-            }
-            stack_nodes[stack_top++] = root->right;
-        }
-        if (root->left != NULL) {
-            
-        }
+void check_capacity(int current_index) {
+    if (current_index == MAX_NODES) {
+        fprintf(stderr, "Maximum capacity of statically allocated array reached!\n");
+        exit(EXIT_FAILURE);
     }
 }
