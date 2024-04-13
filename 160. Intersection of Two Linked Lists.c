16c453814f55e99fe39baa44c1a981fcb6859964
Kristian Koribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-04-13 12:09:08 +0200

    problem: 160
    Much more elegant solution. The idea is to iterate through both the lists A + B and B + A simultaneously.
    
    Romantic two pointers
    "If I wander to the end of the world and still cannot find you, I will take another path and search anew."
    "And I shall do the same, my love."

--- 160. Intersection of Two Linked Lists.c
+++ 160. Intersection of Two Linked Lists.c
@@ -6,46 +6,14 @@
  * };
  */
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-    if (headA == NULL || headB == NULL) {
-        return NULL;
-    }
-    
+
     struct ListNode *node_a = headA;
     struct ListNode *node_b = headB;
-    int len_a = 1;
-    int len_b = 1;
 
-    while(node_a->next != NULL || node_b->next != NULL) {
-        if (node_a->next != NULL) {
-            node_a = node_a->next;
-            len_a++;
-        }
-        if (node_b->next != NULL) {
-            node_b = node_b->next;
-            len_b++;
-        }
+    while(node_a != node_b) {
+        node_a = node_a == NULL ? headB : node_a->next;
+        node_b = node_b == NULL ? headA : node_b->next;
     }
-
-    if (node_a != node_b) {
-        return NULL;
-    }
-
-    int wait_a = len_b > len_a ? len_b - len_a : 0;
-    int wait_b = len_a > len_b ? len_a - len_b : 0;
-
-    node_a = headA;
-    node_b = headB;
-    while (node_a != node_b) {
-        if (wait_a > 0) {
-            wait_a--;
-        } else {
-            node_a = node_a->next;
-        }
-        if (wait_b > 0) {
-            wait_b--;
-        } else {
-            node_b = node_b->next;
-        }
-    }
+    
     return node_a;
 }
