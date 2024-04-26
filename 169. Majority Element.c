aa5111a2eb21abe5e3e1a4c54587e4467227397b
Kristian Koribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-04-27 00:03:16 +0200

    fix: 169
    Remove unused function declaration and comments.

--- 169. Majority Element.c
+++ 169. Majority Element.c
@@ -1,8 +1,6 @@
 void merge_sort(int *arr, int left, int right);
 void merge(int *arr, int left_1, int right_1, int left_2, int right_2);
 
-void print_arr(int *arr, int size);
-
 int majorityElement(int* nums, int numsSize) {
 
     merge_sort(nums, 0, numsSize - 1);
@@ -27,10 +25,8 @@ void merge(int *arr, int left_1, int right_1, int left_2, int right_2) {
     int merged_size = right_2 - left_1 + 1;
     int merged_start = left_1;
     
-    // allocate auxiliary array for merging
     int merged[merged_size];
     
-    // merge the halves, effectively sorting the subarray
     for (int i = 0; i < merged_size; i++) {
         if (left_1 > right_1) {
             merged[i] = arr[left_2++];
@@ -38,12 +34,11 @@ void merge(int *arr, int left_1, int right_1, int left_2, int right_2) {
         else if (left_2 > right_2) {
             merged[i] = arr[left_1++];
         }
-        else {
+        else { 
             merged[i] = arr[left_1] < arr[left_2] ? arr[left_1++] : arr[left_2++];
         }
     }
     
-    // copy merged values back into the original array
     for (int i = 0; i < merged_size; i++) {
         arr[merged_start++] = merged[i];
     }
