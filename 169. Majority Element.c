f513755d5fc5d9ad4595344b457a2f5c436212f0
Kristian Koribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-04-26 23:59:03 +0200

    problem: 169
    My solution with O(nLogn) overall time complexity and O(n) overall space complexity utilizing Merge sort. There is a solution using just O(n) of time and O(1) of space according to problem descriptionm, but I just haven't come up with any yet.

---
+++ 169. Majority Element.c
@@ -0,0 +1,58 @@
+void merge_sort(int *arr, int left, int right);
+void merge(int *arr, int left_1, int right_1, int left_2, int right_2);
+
+void print_arr(int *arr, int size);
+
+int majorityElement(int* nums, int numsSize) {
+
+    merge_sort(nums, 0, numsSize - 1);
+
+    return nums[numsSize / 2];
+}
+
+void merge_sort(int *arr, int left, int right)  {
+    if (left >= right) {
+        return;
+    }
+
+    int mid = (left + right) / 2;
+
+    merge_sort(arr, left, mid);
+    merge_sort(arr, mid + 1, right);
+
+    merge(arr, left, mid, mid + 1, right); 
+}
+
+void merge(int *arr, int left_1, int right_1, int left_2, int right_2) {
+    int merged_size = right_2 - left_1 + 1;
+    int merged_start = left_1;
+    
+    // allocate auxiliary array for merging
+    int merged[merged_size];
+    
+    // merge the halves, effectively sorting the subarray
+    for (int i = 0; i < merged_size; i++) {
+        if (left_1 > right_1) {
+            merged[i] = arr[left_2++];
+        }
+        else if (left_2 > right_2) {
+            merged[i] = arr[left_1++];
+        }
+        else {
+            merged[i] = arr[left_1] < arr[left_2] ? arr[left_1++] : arr[left_2++];
+        }
+    }
+    
+    // copy merged values back into the original array
+    for (int i = 0; i < merged_size; i++) {
+        arr[merged_start++] = merged[i];
+    }
+}
+
+void print_arr(int *arr, int size) {
+    printf("[");
+    for (int i = 0; i < size - 1; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("%d]\n", arr[size - 1]);
+}
