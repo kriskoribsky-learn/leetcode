1a123ad98aa3781239d6fa515e522766d90fc8a7
Kristian Koribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-08-24 10:40:52 +0200

    problem: 206
    Improving upon my previous iterative solution utilizing 3 pointers (previous, current and next) instead of the more complex stack data structure.

--- 206. Reverse Linked List.c
+++ 206. Reverse Linked List.c
@@ -5,37 +5,24 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode** stack_realloc(struct ListNode **stack, size_t size) {
-    struct ListNode **new_stack = realloc(stack, size * sizeof(struct ListNode*));
-    if (new_stack == NULL) {
-        fprintf(stderr, "Stack memory allocation error.\n");
-        free(stack);
-        exit(EXIT_FAILURE);
-    }
-    return new_stack;
-}
-
-struct ListNode* reverseList(struct ListNode* head) {
+struct ListNode *reverseList(struct ListNode* head) {
     if (head == NULL) return NULL;
+    if (head->next == NULL) return head;
 
-    size_t stack_size = 100;
-    size_t stack_top = 0;
-    struct ListNode **stack = stack_realloc(NULL, stack_size);
+    struct ListNode *prev = head;
+    struct ListNode *curr = head->next;
+    struct ListNode *next = head->next->next;
+
+    while (next != NULL) {
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+        next = next->next;
+    }
+
+    curr->next = prev; // begin linked list
+    head->next = NULL; // end linked list
     
-    // push nodes onto the stack
-    for (struct ListNode *node = head; node != NULL; node = node->next) {
-        if (stack_top >= stack_size) {
-            stack = stack_realloc(stack, stack_size *= 2);
-        }
-        stack[stack_top++] = node;
-    }
-
-    // pop nodes from the stack
-    for (struct ListNode *node = head = stack[--stack_top]; stack_top > 0; node = stack[--stack_top]) {
-        node->next = stack[stack_top - 1];
-    }
-
-    stack[0]->next = NULL;
-    free(stack);
-    return head;
+    return curr;
 }
