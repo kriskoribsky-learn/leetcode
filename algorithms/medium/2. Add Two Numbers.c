690f9dc46da129f587464514bd0028903ad679f6
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2024-09-15 19:21:18 +0200

    problem: 2 This solution to this problem ended up being really trivial in the end, I quickly came up with this effective algorithm utilizing base10 adding algorithm with sum of 2 digits and remainder carried onto the higher order.

--- algorithms/medium/2. Add Two Numbers.c
+++ algorithms/medium/2. Add Two Numbers.c
@@ -6,39 +6,31 @@
  * };
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    unsigned long long num1 = 0, num2 = 0;
-    unsigned long long base1 = 1, base2 = 1;
+    struct ListNode* head = NULL;
+    struct ListNode* curr = NULL;
+    int remainder = 0;
     
-    while(l1 != NULL || l2 != NULL) {
+    while (l1 != NULL || l2 != NULL || remainder > 0) {
+        int sum = 0;
         if (l1 != NULL) {
-            num1 += base1 * l1->val;
-            base1 *= 10;
+            sum += l1->val;
             l1 = l1->next;
         }
         if (l2 != NULL) {
-            num2 += base2 * l2->val;
-            base2 *= 10;
+            sum += l2->val;
             l2 = l2->next;
         }
-    }
-    unsigned long long sum = num1 + num2;
-
-    printf("sum: %llu\n");
-
-    struct ListNode *res_head = malloc(sizeof(struct ListNode));
-    res_head->val = sum % 10;
-    res_head->next = NULL;
-    sum /= 10;
-
-    struct ListNode *res_curr = res_head;
-    while (sum > 0) {
-        struct ListNode *tmp = malloc(sizeof(struct ListNode));
-        tmp->val = sum % 10;
+        struct ListNode* tmp = malloc(sizeof(struct ListNode));
+        tmp->val = (sum + remainder) % 10;
         tmp->next = NULL;
-        sum /= 10;
 
-        res_curr->next = tmp;
-        res_curr = tmp;
+        if (head == NULL) {
+            head = curr = tmp;
+        } else {
+            curr->next = tmp;
+            curr = tmp;
+        }
+        remainder = (sum + remainder) / 10;
     }
-    return res_head;
+    return head;
 }
